e54fd900913e3d02c5b86e8c22531c7b1cb93240
Radosław Juszkiewicz <rjuszkiewicz@INF.UG.EDU.PL>
2025-10-14 15:59:22 +0200

    Added Feature 4

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -22,3 +22,31 @@ class TestAccount:
     def test_pesel_non_numeric(self):
         account = Account("John", "Doe", "12345678911")
         assert account.pesel == "12345678911"
+
+
+
+    def test_promo_code_valid(self):
+        account = Account("John", "Doe", "12345678911", "PROM_12a")
+        assert account.balance == 50.0
+    
+
+    def test_promo_code_invalid_format(self):
+        account = Account("John", "Doe", "12345678911", "1_2345678")
+        assert account.balance == 0.0
+
+    def test_promo_code_wrong_prefix(self):
+        account = Account("John", "Doe", "12345678911", "PAOM_123")
+        assert account.balance == 0.0
+
+    def test_promo_code_wrong_suffix(self):
+        account = Account("John", "Doe", "12345678911", "PROM_1234")
+        assert account.balance == 0.0
+
+    def test_promo_code_wrong_suffix_too_long(self):
+        account = Account("John", "Doe", "12345678911", "PROM_1234")
+        assert account.balance == 0.0
+
+
+    def test_promo_code_wrong_suffix_too_short(self):
+        account = Account("John", "Doe", "12345678911", "PROM_1234")
+        assert account.balance == 0.0
